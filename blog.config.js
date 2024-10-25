bb964d56d2a054d670e5db9f3d24df8bc1a44ac0
Jairo Guo <40856078+JairoGuo@users.noreply.github.com>
2024-10-25 23:32:13 +0800

    update

--- blog.config.js
+++ blog.config.js
@@ -29,7 +29,7 @@ const BLOG = {
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
   socialLink: {
-    github: 'https://github.com/JairoGuo'
+    github: 'https://github.com/JairoGuo',
   },
   seo: {
     keywords: ['Jairo', 'Blog'],
